59058c9de313e87e440fd44d1bab065eea4c02a4
Joselyne Uwingabire <uwingajoselyne@gmail.com>
2023-01-17 22:02:02 +0100

    checking if Email and password exist in local storage

--- js/login.js
+++ js/login.js
@@ -1,2 +1,12 @@
-
+function saveLoginData(){
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    if(!localStorage.getItem(email)){
+        localStorage.setItem(email,password);
+        console.log("Email and Password is saved in localstorage");
+    }
+    else{
+        console.log("Email and password exist in local storage");
+    }
+}
 
