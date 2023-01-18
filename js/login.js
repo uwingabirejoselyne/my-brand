1ccfb20841ad9e351a834bf20878ed2cf1ead74c
Joselyne Uwingabire <uwingajoselyne@gmail.com>
2023-01-18 14:34:45 +0100

    checking if email and password exist in array of signup form of localstorage

--- js/login.js
+++ js/login.js
@@ -1,14 +1,16 @@
-function saveLoginData() {
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-    if (email && password) {
-        if (localStorage.getItem(email)) {
-            window.location.href = '../admin/new-post.html';
-        }
-        else {
-            console.log("Email and password is not available in localStorage");
-        }
-    }
 
+let signupData = JSON.parse(localStorage.getItem("signupData")) || [];
+
+function saveLoginData() {
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+  if (email && password) {
+    const match = signupData.find(user => user.email === email && user.password === password);
+    if (match) {
+      window.location.href = '../admin/new-post.html';
+    } else {
+      console.log("Email and password not found in localStoge");
+    }
+  }
 }
 
